351a3934c69d59df8a70eba711a9da8f45232706
Ciro Randazzo <cirorandazzo13@gmail.com>
2024-09-13 13:28:53 -0700

    refactor callback data loading & cutting

--- deepsqueak.py
+++ deepsqueak.py
@@ -1,27 +1,25 @@
-# ./utils/deepsqueak.py
+# deepsqueak.py
 # 2024.05.13 CDR
 #
-# Functions related to loading & preprocessing call labels from deepsqueak
+# Functions related to loading & preprocessing callback data
+# 
+# To be renamed callbacks.py
 
-acceptable_call_labels = [
-    "Call",
-    "Stimulus",
-]  # any stimulus_trials containing call types NOT in this list are excluded (this includes unlabeled!!)
-
-ESA_LOOKUP = {"c": "Call", "s": "Stimulus", "n": "Song"}
+ESA_LOOKUP = {"c": "Call", "s": "Stimulus", "n": "Song", "z": "Song"}
 
 
 def call_mat_stim_trial_loader(
     file,
-    acceptable_call_labels=acceptable_call_labels,
+    acceptable_call_labels=["Call", "Stimulus"],
+    from_notmat=False,
+    min_latency=0,
     calls_index_name="calls_index",
     stims_index_name="stims_index",
-    min_latency=0,
-    from_notmat=False,
+    stim_type_label="Stimulus",
     verbose=True,
 ):
     """
-    TODO: document
+    Given (1) a .mat from DeepSqueak or (2) a .not.mat from evsonganaly, make a trial-by-trial dataframe of callbacks.
     """
     import numpy as np
     import pandas as pd
@@ -32,28 +30,56 @@ def call_mat_stim_trial_loader(
         print(f"Reading file: {file}")
     data = read_mat(file)
 
-    if from_notmat:
-        calls = pd.DataFrame()
+    calls = _read_calls_from_mat(data, from_notmat=from_notmat)
+    file_info = _read_file_info_from_mat(data, from_notmat=from_notmat)
 
+    del data  # don't store twice, it's already saved elsewhere
+
+    # new dataframe, each row maps to a single stimulus + its reponses
+    if verbose:
+        print("Constructing stim trial dataframe")
+
+    stim_trials = construct_stim_trial_df(
+        calls,
+        audio_duration=file_info["wav_duration_s"],
+        stim_type_label=stim_type_label
+        calls_index_name=calls_index_name,
+        stims_index_name=stims_index_name,
+    )
+
+    # reject rows of stim_trials with bad call types
+    if verbose:
+        print(f"Rejecting call types not in: {acceptable_call_labels}")
+
+    stim_trials, rejected_trials, call_types = reject_stim_trials(
+        stim_trials,
+        calls,
+        acceptable_call_labels += stim_type_label,  # you can't just reject stimuli...
+    )
+
+    if verbose:
+        print(call_types)
+
+    return calls, stim_trials, rejected_trials, file_info, call_types
+
+
+def _read_calls_from_mat(
+    data,
+    from_notmat=from_notmat,
+) -> pd.DataFrame:
+    """
+    Reads calls from a .mat containing callback labels (either deepsqueak or evsonganaly .not.mat)
+    """
+    if from_notmat:
         calls = pd.DataFrame()
         calls["start_s"] = data["onsets"] / 1000
         calls["end_s"] = data["offsets"] / 1000
         calls["duration_s"] = calls["end_s"] - calls["start_s"]
         calls["type"] = [ESA_LOOKUP.get(l, l) for l in data["labels"]]
 
-        # TODO: deal with file info
-        file_info = dict(
-            wav_duration_s=np.inf,
-            wav_filename=None,
-            # birdname=None,
-            # d=-1,
-            # block=-1,
-        )
-
     else:
-        assert all([x in data.keys() for x in ["Calls", "file_info"]])
+        assert "Calls" in data.keys()
 
-        file_info = data["file_info"]
         calls = pd.DataFrame(data["Calls"])
         calls = calls[["start_s", "end_s", "duration_s", "type"]]  # reorder columns
 
@@ -67,16 +93,68 @@ def call_mat_stim_trial_loader(
     calls["ici"] = calls.loc[ii]["time_from_prev_onset_s"]
     calls.index.name = calls_index_name
 
-    del data  # don't store twice, it's already saved elsewhere
+    return calls
 
-    stim_trials, rejected_trials, call_types = _construct_stim_trial_df(
-        calls,
-        file_info["wav_duration_s"],
-        acceptable_call_labels,
-        verbose,
+
+def _read_file_info_from_mat(
+    data,
+    from_notmat=from_notmat,
+) -> dict:
+    """
+    Reads file metadata from a .mat containing callback labels (either deepsqueak or evsonganaly .not.mat)
+    """
+
+    if from_notmat:
+        # TODO: deal with file info
+        file_info = dict(
+            wav_duration_s=np.inf,
+            wav_filename=None,
+            # birdname=None,
+            # d=-1,
+            # block=-1,
+        )
+
+    else:
+        assert "file_info" in data.keys()
+        file_info = data["file_info"]
+
+    return file_info
+
+
+def construct_stim_trial_df(
+    calls,
+    audio_duration,
+    stim_type_label,
+    calls_index_name="calls_index",
+    stims_index_name="stims_index",
+):
+    """
+    TODO: document
+    """
+    import pandas as pd
+    import numpy as np
+
+    stims = calls[calls["type"] == stim_label]
+
+    stim_trials = pd.DataFrame()
+
+    # trial start: stimulus onset
+    stim_trials["trial_start_s"] = stims["start_s"]
+
+    # trial end: onset of following stimulus
+    trial_ends = list(stims.start_s[1:])
+    trial_ends.append(audio_duration)
+    stim_trials["trial_end_s"] = pd.Series(trial_ends, index=stims.index)
+
+    stim_trials["stim_duration_s"] = stims["duration_s"]
+
+    # get all labeled 'calls' in this range (may include song, wing flaps, etc)
+    get_calls = lambda row: _get_calls_in_range(
+        calls, row["trial_start_s"], row["trial_end_s"], exclude_stimulus=True
     )
 
-    ## additional fields
+    stim_trials["calls_in_range"] = stim_trials.apply(get_calls, axis=1)
+
     stim_trials["call_times_stim_aligned"] = stim_trials.apply(
         _get_call_times,
         calls_df=calls,
@@ -99,75 +177,17 @@ def call_mat_stim_trial_loader(
         np.min(trial) if len(trial) > 0 else np.nan for trial in onsets
     ]
 
+    stim_trials["wav_filename"] = file_info["wav_filename"]
     # stim_trials['latency_s'] = [np.min(calls[:,0]) if len(calls)>0 else np.nan for calls in stim_trials['call_times_stim_aligned']]
 
     # NOTE: does not just count call indices in `calls_in_range`, which can include calls that have onset before stimulus.
 
-    stim_trials["wav_filename"] = file_info["wav_filename"]
 
     # reindex stim trials by stim # in block, but store call # for each stim
     stim_trials[calls_index_name] = stim_trials.index
     stim_trials.index = pd.Index(range(len(stim_trials)), name=stims_index_name)
 
-    if verbose:
-        print(call_types)
-
-    return calls, stim_trials, rejected_trials, file_info, call_types
-
-
-def _construct_stim_trial_df(
-    calls, audio_duration, acceptable_call_labels, verbose=True
-):
-    """
-    TODO: document
-    """
-    import pandas as pd
-    import numpy as np
-
-    if verbose:
-        print("Constructing stim trial dataframe")
-
-    stims = calls[calls["type"] == "Stimulus"]
-
-    stim_trials = pd.DataFrame()
-
-    # trial start: stimulus onset
-    stim_trials["trial_start_s"] = stims["start_s"]
-
-    # trial end: onset of following stimulus
-    trial_ends = list(stims.start_s[1:])
-    trial_ends.append(audio_duration)
-    stim_trials["trial_end_s"] = pd.Series(trial_ends, index=stims.index)
-
-    stim_trials["stim_duration_s"] = stims["duration_s"]
-
-    # get all labeled 'calls' in this range (may include song, wing flaps, etc)
-    get_calls = lambda row: _get_calls_in_range(
-        calls, row["trial_start_s"], row["trial_end_s"], exclude_stimulus=True
-    )
-
-    stim_trials["calls_in_range"] = stim_trials.apply(get_calls, axis=1)
-
-    # do rejections (exclude trial if range includes call types other than those in acceptable_call_labels)
-    call_types = stim_trials["calls_in_range"].apply(
-        lambda x: calls["type"].loc[x].value_counts()
-    )  # get call types
-
-    keep_columns = [c for c in call_types.columns if c in acceptable_call_labels]
-    call_types_rejectable = call_types.drop(
-        columns=keep_columns
-    )  # get only values of 'rejectable' calls
-
-    to_reject = call_types_rejectable.apply(np.any, axis=1)
-
-    rejected_trials = stim_trials[to_reject]
-    stim_trials = stim_trials[~to_reject]
-
-    if verbose:
-        print(f"\t- # trials rejected: {len(rejected_trials)}")
-        print(f"\t- # trials accepted: {len(stim_trials)}")
-
-    return stim_trials, rejected_trials, call_types
+    return stim_trials
 
 
 def _get_calls_in_range(calls, range_start, range_end, exclude_stimulus=True):
@@ -224,52 +244,32 @@ def _get_call_times(trial, calls_df, stimulus_aligned=True):
     return call_times_stim_aligned
 
 
-def multi_index_from_dict(df, index_dict, keep_current_index=True):
+def reject_stim_trials(
+    stim_trials,
+    calls,
+    acceptable_call_labels,
+):
     """
-    TODO: documentation
+    Given stim-aligned dataframe, exclude all trials with call types other than those in acceptable_call_labels)
     """
-    df_indexed = df.copy()
+    call_types = stim_trials["calls_in_range"].apply(
+        lambda x: calls["type"].loc[x].value_counts()
+    )  # get call types
 
-    for k, v in index_dict.items():
-        df_indexed[k] = v
+    keep_columns = [c for c in call_types.columns if c in acceptable_call_labels]
+    call_types_rejectable = call_types.drop(
+        columns=keep_columns
+    )  # get only values of 'rejectable' calls
 
-    index_keys = list(index_dict.keys())
-    if keep_current_index:
-        index_keys.append(df.index)
+    to_reject = call_types_rejectable.apply(np.any, axis=1)
 
-    df_indexed.set_index(index_keys, inplace=True)
+    rejected_trials = stim_trials[to_reject]
+    stim_trials = stim_trials[~to_reject]
 
-    return df_indexed
+    if verbose:
+        print(f"\t- # trials rejected: {len(rejected_trials)}")
+        print(f"\t- # trials accepted: {len(stim_trials)}")
+
+    return stim_trials, rejected_trials, call_types
 
 
-if __name__ == "__main__":
-    import glob
-
-    processed_directory = (
-        "/Users/cirorandazzo/code/callback-analysis/data/processed_mats/*.mat"
-    )
-    acceptable_call_labels = [
-        "Call",
-        "Stimulus",
-    ]  # any stimulus_trials containing call types NOT in this list are excluded (this includes unlabeled, which are stored as 'USV'!!)
-
-    files = [f for f in glob.glob(processed_directory)]
-
-    # TODO: deal with issues in these files.
-    # files = [  # FIRST ONE WORKS
-    #     '/Users/cirorandazzo/code/callback-analysis/data/processed_mats/pk81rd39-d2-20240403122742-Block6-PROCESSED.mat',
-    #     '/Users/cirorandazzo/code/callback-analysis/data/processed_mats/or91rd13-d2-20240712120331-Stim0-Block0 2024-07-15 11_40 AM-PROCESSED.mat',
-    #     '/Users/cirorandazzo/code/callback-analysis/data/processed_mats/or91rd13-d1-20240711115800-Stim0-Block1 2024-07-15  8_27 AM-PROCESSED.mat'
-    # ]
-
-    print(f"Processing {len(files)} files...")
-    for file in files:
-        try:
-            calls_df, stim_trials, rejected_trials, file_info, call_types = (
-                call_mat_stim_trial_loader(file, verbose=False)
-            )
-
-            # print(file_info.keys())
-        except Exception as e:
-            print(f"Error processing {file}")
-            print(e)
