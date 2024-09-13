303fafe7f1de337209507c07b929ff1ba968dbca
Ciro Randazzo <cirorandazzo13@gmail.com>
2024-09-13 13:29:55 -0700

    rename deepsqueak.py to callback.py

---
+++ file.py
@@ -0,0 +1,22 @@
+# file.py
+# 
+# 
+
+def multi_index_from_dict(df, index_dict, keep_current_index=True):
+    """
+    Add Multiindex columns to a pd dataframe given values from a dict. Useful before merging dataframes from separate timepoints, for example.
+    """
+    import pandas as pd
+
+    df_indexed = df.copy()
+
+    for k, v in index_dict.items():
+        df_indexed[k] = v
+
+    index_keys = list(index_dict.keys())
+    if keep_current_index:
+        index_keys.append(df.index)
+
+    df_indexed.set_index(index_keys, inplace=True)
+
+    return df_indexed
