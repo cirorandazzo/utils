4f79138df8f797179a029ae60a8ddcacc8784bb1
Ciro Randazzo <cirorandazzo13@gmail.com>
2025-03-05 11:34:00 -0800

    enable umap plot_embedding_data for all_trials df

--- utils/umap.py
+++ utils/umap.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import hdbscan
 
 from .file import parse_birdname
-
+from .plot import remap_cmap
 
 def get_time_since_stim(x, all_trials):
     """
@@ -46,13 +46,13 @@ def loc_relative(wav_filename, calls_index, df, field="index", i=1, default=None
 def plot_embedding_data(
     embedding,
     embedding_name,
-    all_breaths,
+    df,
+    ax=None,
     plot_type="putative_call",
-    scatter_kwargs=None,
-    set_kwargs=None,
     show_colorbar=True,
     cmap_name=None,
-    ax=None,
+    scatter_kwargs=None,
+    set_kwargs=None,
     **kwargs,
 ):
     """
@@ -60,7 +60,7 @@ def plot_embedding_data(
 
     Parameters:
     - embedding_name: Name of the embedding.
-    - all_breaths: DataFrame containing the relevant data.
+    - df: DataFrame containing the metadata for cmap. Order should match embedding. Can be all_breaths or all_trials.
     - scatter_kwargs: Keyword arguments for the scatter plot.
     - set_kwargs: Settings for the plot (e.g., axis limits, labels).
     - plot_type: Type of plot to generate ("putative_call", "amplitude", "duration", "time_since_stim").
@@ -84,6 +84,8 @@ def plot_embedding_data(
             ylabel="UMAP2",
         )
 
+    set_kwargs = {**set_kwargs}  # copy set_kwargs; don't modify input
+
     if "title" not in set_kwargs.keys():
         set_kwargs["title"] = f"{embedding_name}: {plot_type.upper()}"
 
@@ -91,17 +93,26 @@ def plot_embedding_data(
         default_cmaps = {
             "putative_call": "Dark2_r",
             "amplitude": "magma_r",
-            "duration": "magma_r",
+            "duration": "cool",
             "breaths_since_stim": "viridis_r",
             "bird_id": "Set2",
+            "insp_onset": "RdYlGn",
+            "insp_offset": "magma_r",
         }
 
-        cmap_name = default_cmaps[plot_type]
+        cmap_name = default_cmaps.get(plot_type, "viridis")
 
     # GET PLOT TYPE SPEIFICS
-    if plot_type == "putative_call":
+    if "c" in kwargs.keys():
+        # manually defined color labels
         plot_type_kwargs = dict(
-            c=np.array(all_breaths["putative_call"]).astype(int),
+            c=kwargs.pop("c"),
+            cmap=cmap_name,
+        )
+
+    elif plot_type == "putative_call":
+        plot_type_kwargs = dict(
+            c=np.array(df["putative_call"]).astype(int),
             cmap=plt.get_cmap(cmap_name, 2),
             vmin=0,
             vmax=1,
@@ -113,24 +124,35 @@ def plot_embedding_data(
 
     elif plot_type == "amplitude":
         plot_type_kwargs = dict(
-            c=all_breaths.amplitude,
+            c=df.amplitude,
             cmap=cmap_name,
         )
         cbar_label = "amplitude (normalized)"
 
     elif plot_type == "duration":
+        if "duration_s" in df.columns:
+            c = df.duration_s * 1000
+        elif "ii_first_insp" in df.columns:
+            try:
+                all_insps = np.vstack(df["ii_first_insp"]).T / kwargs.pop("fs") * 1000
+            except KeyError as e:
+                raise KeyError("Pass fs as a kwarg to compute duration from column ii_first_insp")
+            c = all_insps[1, :] - all_insps[0, :]
+        else:
+            raise ValueError("Inputted df needs one column of [duration_s, ii_first_insp] to plot duration.")
+
         plot_type_kwargs = dict(
-            c=all_breaths.duration_s,
+            c=c,
             cmap=cmap_name,
-            vmax=1.0,
+            vmax=1000,
         )
-        cbar_label = "duration (s)"
+        cbar_label = "duration (ms)"
 
     elif plot_type == "breaths_since_stim":
         # by default 8+ breaths are merged on cmap
         n_breaths = kwargs.pop("n_breaths", 8)
 
-        n_since_stim = all_breaths["trial_index"].fillna(-1)
+        n_since_stim = df["trial_index"].fillna(-1)
         cmap = plt.get_cmap(cmap_name, n_breaths+1)
         cmap.set_bad("k")
 
@@ -147,7 +169,7 @@ def plot_embedding_data(
         cbar_tick_labels[-1] = f"{cbar_tick_labels[-1]}+"
 
     elif plot_type == "bird_id":
-        birdnames = pd.Categorical(all_breaths.apply(lambda x: parse_birdname(x.name[0]), axis=1))
+        birdnames = pd.Categorical(df.apply(lambda x: parse_birdname(x.name[0]), axis=1))
         n_birds = len(birdnames.unique())
 
         cmap = plt.get_cmap(cmap_name, n_birds)
@@ -161,6 +183,30 @@ def plot_embedding_data(
         cbar_ticks = np.arange(n_birds)
         cbar_tick_labels = birdnames.unique()
         cbar_label = None
+
+    elif plot_type in ["insp_onset", "insp_offset"]:
+        assert "fs" in kwargs.keys(), f"fs must be provided as kwarg for {plot_type} embedding plot."
+        all_insps = np.vstack(df["ii_first_insp"]).T / kwargs.pop("fs") * 1000
+
+        if plot_type == "insp_onset":
+            onsets = all_insps[0, :]
+            # map time 0 --> center of cmap
+            cmap_zero = abs(onsets.min()) / (np.ptp(onsets))
+
+            plot_type_kwargs = dict(
+                c=onsets,
+                vmin=onsets.min(),
+                vmax=onsets.max(),
+                cmap=remap_cmap(cmap_name, midpoint=cmap_zero),
+            )
+
+        elif plot_type == "insp_offset":
+            plot_type_kwargs = dict(c=all_insps[1, :], cmap=cmap_name)
+
+        else:
+            raise KeyError("Invalid plot type. How'd you even get here?")
+
+        cbar_label = f"{plot_type} (ms, stim_aligned)"
 
     else:
         raise ValueError(f"Unsupported plot type: {plot_type}")
@@ -178,11 +224,11 @@ def plot_embedding_data(
     ax.set(**set_kwargs)
 
     # COLORBAR
-    if show_colorbar:
+    if show_colorbar and "cbar_label" in vars():
         cbar = plt.colorbar(sc, ax=ax)
         cbar.set_label(cbar_label)
 
-    if 'cbar_ticks' in vars():
+    if "cbar_ticks" in vars():
         cbar.set_ticks(cbar_ticks)
         cbar.set_ticklabels(cbar_tick_labels)
 
