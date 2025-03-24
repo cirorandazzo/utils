ac0455c5676930a8075c3d82129992eadf3c04b8
Ciro Randazzo <ciro.randazzo@ucsf.edu>
2025-03-24 14:30:31 -0700

    document get_phase and clean up parameter names

--- utils/breath.py
+++ utils/breath.py
@@ -512,36 +512,44 @@ def get_first_breath_segment(
     return earliest_call
 
 
-def get_phase(t_nMin1Exp_to_Call, avgExpDur, avgInspDur):
+def get_phase(
+    breathDur,
+    avgExpDur,
+    avgInspDur,
+):
     """
     python implementation of ziggy phase computation code ("phase2.m")
 
-    t_nMin1Exp_to_Call: time between preceding expiration & call [waiting to hear from ziggy what event is meant by "call"]
+    Note: algorithm errors on breathDur > 2 normal breath lengths (for consistency with ZK code)
+
+    breathDur: time between preceding expiration & event for which to compute phase - usually onset of call expiration. (formerly t_nMin1Exp_to_Call)
+    avgExpDur: duration of expiration for comparison (in same unit as breathDur). Usually, this is the mean duration of non-call expirations. breathDur in [0, avgExpDur] (time) is mapped to [0, pi].  breathDur in [0, avgExpDur] (time) is mapped to [0, pi]. 
+    avgInspDur: duration of inspiration for comparison (in same unit as breathDur). Usually, this is the mean duration of non-call inspirations. breathDur in [0, avgExpDur] (time) is mapped to [0, pi].  breathDur in [avgExpDur, avgExpDur+avgInspDur] (time) is mapped to [pi, 2pi].
     """
 
     phase = None
 
     avgBreathDur = avgExpDur + avgInspDur
 
-    assert t_nMin1Exp_to_Call < (
+    assert breathDur < (
         2 * avgBreathDur
     ), "algorithm only implmented for callT within 2 normal breath lengths!"
 
-    t_nMin1Exp_to_Call = t_nMin1Exp_to_Call % avgBreathDur
+    breathDur = breathDur % avgBreathDur
 
     # call happens before the expiration before the call... (ie, oops)
-    if t_nMin1Exp_to_Call < 0:
+    if breathDur < 0:
         phase = 0.1
 
     # call happens during this expiration
-    elif t_nMin1Exp_to_Call < avgExpDur:
+    elif breathDur < avgExpDur:
         # expiration is [0, pi]
-        phase = np.pi * (t_nMin1Exp_to_Call / avgExpDur)
+        phase = np.pi * (breathDur / avgExpDur)
 
     # call happens during inspiration after that
-    elif t_nMin1Exp_to_Call >= avgExpDur and t_nMin1Exp_to_Call < avgBreathDur:
+    elif breathDur >= avgExpDur and breathDur < avgBreathDur:
         # inspiration is [pi, 2pi]
-        phase = np.pi * (1 + (t_nMin1Exp_to_Call - avgExpDur) / avgInspDur)
+        phase = np.pi * (1 + (breathDur - avgExpDur) / avgInspDur)
 
     else:
         ValueError("this really shouldn't happen...")
